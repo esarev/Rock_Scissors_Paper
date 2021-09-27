70b2d468a3fb1716778306123c58a8920c896266
Alex <esarev@inbox.ru>
2021-09-27 17:40:30 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', function() {
+
+  let countUser = document.querySelector('.count-user'),
+      countComp = document.querySelector('.count-comp'),
+      userField = document.querySelector('.user-field'),
+      compField = document.querySelector('.comp-field'),
+      sound = document.querySelector('.sound'),
+      play = document.querySelector('.play'),
+      fields = document.querySelectorAll('.fields'),
+      userStep, compStep, countU = 0, countC = 0, blocked = false;
+
+
+  function choiceUser() {
+
+  }
+
+  function choiceComp() {
+
+  }
+
+  function winner() {
+
+  }
+
+  function playGame() {
+
+  }
+
+  play.addEventListener('click', playGame);
+  userField.addEventListener('click', choiceUser);
+
+
+});
